5c9956936a9053af6fe8b31d7ae12efdcbd5d2ed
zian <mynameiszian@gmail.com>
2022-05-31 14:45:49 +0800

    FINALLY FUCKING WORK UPLOADING FILE AND MOVING IT

--- fileUpload.php
+++ fileUpload.php
@@ -1,14 +1,57 @@
 <?php
 
-if (isset($_POST['submit'])){
-    if(!empty($_FILES['image']['name'])){
 
-        print_r($_FILES);
-        echo 'uploaded successfully';
+$message = "select file to upload";
 
-    }else{
-        print_r($_FILES);
-        echo "please choose a file!";
+if (isset($_POST['submit'])) {
+
+
+    $allowed_ext = ['pptx', 'php', 'docs', 'jpg', 'png', 'jpeg'];
+
+
+    if (!empty($_FILES['image']['name'])) {
+
+        $file_name = $_FILES['image']['name'];
+        $file_path = $_FILES['image']['full_path'];
+        $file_type = $_FILES['image']['type'];
+        $file_size = $_FILES['image']['size'] / 1e+6;
+        $file_tmp =  $_FILES['image']['tmp_name'];
+        $target_dir = "uploads/$file_name";
+
+        //split the extension and the actual filename
+        $file_ext = explode('.', $file_name);
+
+        //discard the filename and only take the extension
+        $file_ext = strtolower(end($file_ext));
+
+        
+        var_dump($file_tmp);
+
+        if (in_array($file_ext, $allowed_ext)) {
+
+            if ($file_size > (2 * 1e+6)) {
+
+                $message = "file is too large";
+            } else {
+
+                $move = move_uploaded_file($file_tmp, $target_dir);
+
+                switch ($move) {
+                    case true:
+                        $message = "file uploaded successfully";
+                        break;
+
+                    case false:
+                        $message = "Error uploading file " . var_dump($move);
+                        break;
+                }
+            }
+        } else {
+            $message = "invalid file type";
+        }
+    } else {
+
+        $message = "please choose a file!";
     }
 }
 
@@ -21,25 +64,22 @@ if (isset($_POST['submit'])){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>file upload practice</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="">
 </head>
 
 <body>
-    <form method="POST" action=<?php echo $_SERVER["PHP_SELF"]; ?> enctype="multipart/form-data">
-        <div>
-            <label>selecet file to upload</label><br />
-            <input type="file" name="image"><br />
-            <input type="submit" name="submit">
-            
-        </div>
+    <h1><?php echo $message;  ?></h1>>
+    <form action=<?php echo $_SERVER["PHP_SELF"]; ?> method="POST" enctype="multipart/form-data">
+
+
+        <input type="file" name="image">
+        <input type="submit" value="Submit" name="submit">
+
 
     </form>
-
-
-    <script src="" async defer></script>
 </body>
 
 </html>
